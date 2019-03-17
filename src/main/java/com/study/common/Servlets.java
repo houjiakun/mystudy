b44c38eea9b503e8151c3df9a89a8ccff32e9c92
531852181@qq.com <531852181@qq.com>
2019-03-17 21:56:09 +0800

    获取basePath

---
+++ src/main/java/com/study/common/Servlets.java
@@ -0,0 +1,174 @@
+/**
+ * Copyright (c) 2005-2012 springside.org.cn
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ */
+package com.study.common;
+
+import com.google.common.net.HttpHeaders;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+import java.util.*;
+import java.util.Map.Entry;
+
+/**
+ * Http与Servlet工具类.
+ * @author calvin/thinkgem
+ * @version 2014-8-19
+ */
+public class Servlets {
+    /**
+     * 设置客户端缓存过期时间 的Header.
+     */
+    public static void setExpiresHeader(HttpServletResponse response, long expiresSeconds) {
+        // Http 1.0 header, set a fix expires date.
+        response.setDateHeader(HttpHeaders.EXPIRES, System.currentTimeMillis() + expiresSeconds * 1000);
+        // Http 1.1 header, set a time after now.
+        response.setHeader(HttpHeaders.CACHE_CONTROL, "private, max-age=" + expiresSeconds);
+    }
+
+    /**
+     * 设置禁止客户端缓存的Header.
+     */
+    public static void setNoCacheHeader(HttpServletResponse response) {
+        // Http 1.0 header
+        response.setDateHeader(HttpHeaders.EXPIRES, 1L);
+        response.addHeader(HttpHeaders.PRAGMA, "no-cache");
+        // Http 1.1 header
+        response.setHeader(HttpHeaders.CACHE_CONTROL, "no-cache, no-store, max-age=0");
+    }
+
+    /**
+     * 设置LastModified Header.
+     */
+    public static void setLastModifiedHeader(HttpServletResponse response, long lastModifiedDate) {
+        response.setDateHeader(HttpHeaders.LAST_MODIFIED, lastModifiedDate);
+    }
+
+    /**
+     * 设置Etag Header.
+     */
+    public static void setEtag(HttpServletResponse response, String etag) {
+        response.setHeader(HttpHeaders.ETAG, etag);
+    }
+
+    /**
+     * 根据浏览器If-Modified-Since Header, 计算文件是否已被修改.
+     *
+     * 如果无修改, checkIfModify返回false ,设置304 not modify status.
+     *
+     * @param lastModified 内容的最后修改时间.
+     */
+    public static boolean checkIfModifiedSince(HttpServletRequest request, HttpServletResponse response,
+                                               long lastModified) {
+        long ifModifiedSince = request.getDateHeader(HttpHeaders.IF_MODIFIED_SINCE);
+        if ((ifModifiedSince != -1) && (lastModified < ifModifiedSince + 1000)) {
+            response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 根据浏览器 If-None-Match Header, 计算Etag是否已无效.
+     *
+     * 如果Etag有效, checkIfNoneMatch返回false, 设置304 not modify status.
+     *
+     * @param etag 内容的ETag.
+     */
+    public static boolean checkIfNoneMatchEtag(HttpServletRequest request, HttpServletResponse response, String etag) {
+        String headerValue = request.getHeader(HttpHeaders.IF_NONE_MATCH);
+        if (headerValue != null) {
+            boolean conditionSatisfied = false;
+            if (!"*".equals(headerValue)) {
+                StringTokenizer commaTokenizer = new StringTokenizer(headerValue, ",");
+
+                while (!conditionSatisfied && commaTokenizer.hasMoreTokens()) {
+                    String currentToken = commaTokenizer.nextToken();
+                    if (currentToken.trim().equals(etag)) {
+                        conditionSatisfied = true;
+                    }
+                }
+            } else {
+                conditionSatisfied = true;
+            }
+
+            if (conditionSatisfied) {
+                response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
+                response.setHeader(HttpHeaders.ETAG, etag);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 设置让浏览器弹出下载对话框的Header.
+     *
+     * @param fileName 下载后的文件名.
+     */
+    public static void setFileDownloadHeader(HttpServletResponse response, String fileName) {
+        try {
+            // 中文文件名支持
+            String encodedfileName = new String(fileName.getBytes(), "ISO8859-1");
+            response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + encodedfileName + "\"");
+        } catch (UnsupportedEncodingException e) {
+            e.getMessage();
+        }
+    }
+
+    /**
+     * 组合Parameters生成Query String的Parameter部分,并在paramter name上加上prefix.
+     *
+     */
+    public static String encodeParameterStringWithPrefix(Map<String, Object> params, String prefix) {
+        StringBuilder queryStringBuilder = new StringBuilder();
+
+        String pre = prefix;
+        if (pre == null) {
+            pre = "";
+        }
+        Iterator<Entry<String, Object>> it = params.entrySet().iterator();
+        while (it.hasNext()) {
+            Entry<String, Object> entry = it.next();
+            queryStringBuilder.append(pre).append(entry.getKey()).append("=").append(entry.getValue());
+            if (it.hasNext()) {
+                queryStringBuilder.append("&");
+            }
+        }
+        return queryStringBuilder.toString();
+    }
+
+
+    /**
+     * 获取当前请求对象
+     */
+    public static HttpServletRequest getRequest() {
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (requestAttributes == null) {
+            return null;
+        }
+        return requestAttributes.getRequest();
+    }
+
+    public static HttpServletResponse getResponse() {
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (requestAttributes == null) {
+            return null;
+        }
+        return requestAttributes.getResponse();
+    }
+
+    public static String getContextPath() {
+        HttpServletRequest req = getRequest();
+        return req.getScheme() + "://" + req.getServerName() + ":" + req.getServerPort() + req.getContextPath() + "/";
+    }
+
+    public static String getBasePath(){
+       return getRequest().getSession().getServletContext().getRealPath("model");
+    }
+}
